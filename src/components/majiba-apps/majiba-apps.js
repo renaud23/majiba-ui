edc4c7d04689797faa2f0b495013b4b6591619b5
Renaud Genevois <KQHLZ2@ad.insee.intra>
2020-11-02 08:57:30 +0100

    table style

--- src/components/majiba-apps/majiba-apps.js
+++ src/components/majiba-apps/majiba-apps.js
@@ -1,5 +1,6 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { useRecoilState } from "recoil";
+import classnames from "classnames";
 import { majibaState } from "../../state";
 import Paper from "@material-ui/core/Paper";
 import { getMajibaApps } from "../../commons";
@@ -8,18 +9,24 @@ import Waiting from "../waiting";
 import { ReactLargeTable } from "react-scrollable-div";
 import "./simple-theme.scss";
 
-// name: "web4g", module: "bojs", kind: "application/javascript", env: "prd", ad: "AD\DG75-SNDI-Maintenance-Web4G"
+function ContentCell({ cell }) {
+  const { value, key } = cell;
+  return <span className={classnames("simple-theme-cell", key)}>{value}</span>;
+}
 
 function prepare(apps) {
   const rows = apps.map(function (e) {
-    return { ...e, __height: 30 };
+    const values = Object.entries(e).reduce(function (o, [key, value]) {
+      return { ...o, [key]: { value, key } };
+    }, {});
+    return { ...values, __height: 40 };
   });
 
   return {
     header: [
-      { label: "Nom", path: "name", __width: 100, resizable: true },
-      { label: "module", path: "Module", __width: 100, resizable: true },
-      { label: "Type", path: "kind", __width: 200, resizable: true },
+      { label: "Nom", path: "name", __width: 250, resizable: true },
+      { label: "Module", path: "module", __width: 100, resizable: true },
+      { label: "Type", path: "kind", __width: 300, resizable: true },
       { label: "Groupe AD", path: "ad", __width: 300, resizable: true },
     ],
     rows,
@@ -28,6 +35,7 @@ function prepare(apps) {
 
 function MajibaApps() {
   const [wait, setWait] = useState(false);
+  const [tableData, setTableData] = useState(undefined);
   const [apps, setApps] = useRecoilState(majibaState);
   const refresh = useCallback(
     function () {
@@ -35,7 +43,7 @@ function MajibaApps() {
       getMajibaApps()
         .then(function (apps) {
           setWait(false);
-          setApps(prepare(apps));
+          setApps(apps);
         })
         .catch(function () {
           setWait(false);
@@ -43,7 +51,16 @@ function MajibaApps() {
     },
     [setApps]
   );
-  console.log(apps);
+
+  useEffect(
+    function () {
+      if (apps) {
+        setTableData(prepare(apps));
+      }
+    },
+    [apps]
+  );
+
   return (
     <Paper elevation={2}>
       <Waiting status={wait} />
@@ -53,14 +70,15 @@ function MajibaApps() {
         className="form-button"
         onClick={refresh}
       >
-        {apps && apps.rows.length ? "Rafraichir la liste" : "Charger la liste"}
+        {apps ? "Rafraichir la liste" : "Charger la liste"}
       </Button>
-      {apps && apps.rows.length ? (
+      {tableData && tableData.rows ? (
         <div className="table-apps-container">
           <ReactLargeTable
-            data={apps}
+            data={tableData}
             headerHeight={50}
             className="simple-theme"
+            cellRenderer={ContentCell}
           />
         </div>
       ) : null}
